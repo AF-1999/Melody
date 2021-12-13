4ef5979202374ed4dbc54beae9fc8f164ee2edb9
flippi21 <96062335+flippi21@users.noreply.github.com>
2021-12-13 22:48:37 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+    var currentFloor =2;
+		var counterUp =$(".counter-up");
+		var counterDown =$(".counter-down");
+		var floorPath=$(".home-image path");
+		var modal=$(".modal");
+		var modalClose=$(".modal-close-button");
+		var viewFlatsButton=$(".view-flats");
+
+    floorPath.on("mouseover", function() {
+			currentFloor =$(this).attr("data-floor");
+			floorPath.removeClass("current-floor");
+			$(".counter").text(currentFloor);
+    });
+
+		floorPath.on("click", toggleModal); 
+		modalClose.on("click", toggleModal);
+		viewFlatsButton.on("click", toggleModal);
+
+		counterUp.on("click", function() {
+			if(currentFloor<18){
+				currentFloor++;
+				usCurrentFloor =currentFloor.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})
+				$(".counter").text(usCurrentFloor);	
+				floorPath.removeClass("current-floor");
+				$(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
+			}
+		});
+
+		counterDown.on("click", function() {
+			if(currentFloor>2){
+				currentFloor--;
+				usCurrentFloor =currentFloor.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})
+				$(".counter").text(usCurrentFloor);	
+				floorPath.removeClass("current-floor");
+				$(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
+			}
+		});
+
+		function toggleModal() {
+			modal.toggleClass("is-open");
+		};
+});
